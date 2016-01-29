143ad105739a6392b5432920c773976c5f9a4789
abstractlabs <abstractlabs@gmail.com>
2016-01-28 23:03:36 -0700

    1.0.2 - Fixed bug causing ordinal to be included when member supplied on identification extension

--- ExtendType.OfType/Identification/Extensions.cs
+++ ExtendType.OfType/Identification/Extensions.cs
@@ -9,12 +9,15 @@ namespace ExtendType.OfType.Identification
 {
 	public static class Extensions
 	{
+		/// <summary>
+		/// Converts the supplied type with optional member and ordinal into an identification key
+		/// </summary>
 		public static string ToKey(this Type instance, string member = null, string ordinal = null)
 		{
 			Contract.Requires(instance != null);
 			StringBuilder builder= new StringBuilder(instance.FullName);
 			if (!String.IsNullOrWhiteSpace(member)) builder.Append('.').Append(member);
-			if (!String.IsNullOrWhiteSpace(member)) builder.Append('[').Append(ordinal).Append(']');
+			if (!String.IsNullOrWhiteSpace(member) && !String.IsNullOrWhiteSpace(ordinal)) builder.Append('[').Append(ordinal).Append(']');
 			return builder.ToString();
 		}
 	}
